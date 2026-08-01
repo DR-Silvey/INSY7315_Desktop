c14f5a1fdb3c852adcdf0b39bec636183d2e1759
Damian Silvey <ST10443098@vcconnect.edu.za>
2026-08-01 18:18:37 +0200

    Populated the view used to setup assignments, for now within the prototype no questions can be added yet but a template is still shown

---
+++ EDAAMS_Desktop/Controllers/AssessmentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EDAAMS_Desktop.Controllers
+{
+    public class AssessmentController : Controller
+    {
+        public IActionResult CreateAssessment()
+        {
+            return View("AssessmentBuilders/CreateAssessment");
+
+        }
+
+    }
+}
