34ed20eca9d5d017652fbfc53ff338fa427b598f
Damian Silvey <ST10443098@vcconnect.edu.za>
2026-08-07 11:38:01 +0200

    Added more screens from mobile version to the desktop version, adding students, grades etc.

--- EDAAMS_Desktop/Controllers/HomeController.cs
+++ EDAAMS_Desktop/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace EDAAMS_Desktop.Controllers
             return View();
         }
 
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- EDAAMS_Desktop/obj/Debug/net8.0/EDAAMS_Desktop.AssemblyInfo.cs
+++ EDAAMS_Desktop/obj/Debug/net8.0/EDAAMS_Desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EDAAMS_Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+921bffa2cb5612c29d9e6af3f1a6532699f848f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e197fe7cb29dea2dad9cef35b1635282170dab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EDAAMS_Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EDAAMS_Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
