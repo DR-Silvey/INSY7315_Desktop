c63339e657540fc8252d6c638fabdc268593eb67
Damian Silvey <ST10443098@vcconnect.edu.za>
2026-08-10 12:15:20 +0200

    Styled the pages for students and assessments in terms of positioning, marked the Settings and privacy pages as off limits during the prototype phase

--- EDAAMS_Desktop/obj/Debug/net8.0/EDAAMS_Desktop.AssemblyInfo.cs
+++ EDAAMS_Desktop/obj/Debug/net8.0/EDAAMS_Desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EDAAMS_Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e9733bf7baef3a3e986c7ac143cf12deb73f2c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae38169ab28c8836a3922d91a6437fba2d9f3c20")]
 [assembly: System.Reflection.AssemblyProductAttribute("EDAAMS_Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EDAAMS_Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
