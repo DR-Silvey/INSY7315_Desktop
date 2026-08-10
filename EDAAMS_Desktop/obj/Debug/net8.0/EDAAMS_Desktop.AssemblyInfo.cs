558a99a6d163ccc0bc6618625f27ac2c3fce356e
Damian Silvey <ST10443098@vcconnect.edu.za>
2026-08-10 15:29:19 +0200

    Finalised color scheme for GUI and fixed some placement issues

--- EDAAMS_Desktop/obj/Debug/net8.0/EDAAMS_Desktop.AssemblyInfo.cs
+++ EDAAMS_Desktop/obj/Debug/net8.0/EDAAMS_Desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EDAAMS_Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c63339e657540fc8252d6c638fabdc268593eb67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73fc3674ee123912d87972585790786327544674")]
 [assembly: System.Reflection.AssemblyProductAttribute("EDAAMS_Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EDAAMS_Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
